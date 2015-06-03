a032543ce5ccdd135a0aa8b21f1b569af6d07c56
takeda <take508@hotmail.com>
2015-06-03 11:03:42 +0900

    Button
    Button

---
+++ Button/Assets/Scripts/InstantButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantButton : MonoBehaviour
+{
+    bool push = false; // ボタンが押されているか？
+
+    public void StartPush()
+    {
+        push = true;
+
+        //ここにやらせたい処理を書きます
+        Debug.Log("瞬間！");
+    }
+}
---
+++ Button/Assets/Scripts/RepeatButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepeatButton : MonoBehaviour
+{
+    bool push = false; // ボタンが押されているか？
+
+    public void StartPush()
+    {
+        push = true;
+
+        //ここにやらせたい処理を書きます
+        Debug.Log("押した！");
+    }
+
+    public void StopPush()
+    {
+        push = false;
+        //ここにやらせたい処理を書きます
+        Debug.Log("離されました！");
+    }
+
+    void Update()
+    {
+        if (push)
+        {
+            //ここにやらせたい処理を書きます
+            Debug.Log("押されてます！");
+        }
+    }
+}
